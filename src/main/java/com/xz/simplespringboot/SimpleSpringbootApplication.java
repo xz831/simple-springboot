a5451a1b3234b389dd24cf15f2608255943202b0
xz <842496944@qq.com>
2021-04-05 19:03:31 +0800

    init

---
+++ src/main/java/com/xz/simplespringboot/SimpleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xz.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xz/simplespringboot/SimpleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xz.simplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
